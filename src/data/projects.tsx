41fdd2aa504f10bddabcb7b4508b612a25a83d96
luigicabrera10 <luigi.cabrera.h@hotmail.com>
2026-01-22 22:01:24 -0500

    ✨ Project Section

---
+++ src/data/projects.tsx
@@ -0,0 +1,31 @@
+export const projects = [
+    {
+        'tittle': 'StockEx',
+        'description': 'Designed and developed scalable full-stack applications using React, NextJs, FastAPI, NodeJS, Flutter, and PostgreSQL. Integrated third-party APIs, built intuitive UIs, and deployed production-ready systems.',
+        'img': [
+            '/imgs/projects/stockex/market.jpg',
+            '/imgs/projects/stockex/chart.jpg',
+            '/imgs/projects/stockex/chart.jpg',
+            '/imgs/projects/stockex/panel.jpg',
+            '/imgs/projects/stockex/portfolio.jpg',
+            '/imgs/projects/stockex/profile.jpg',
+        ],
+    },
+    {
+        'tittle': 'AI Content Creation',
+        'description': 'Built end-to-end AI pipelines for automated video creation using AI models and tools, including GPT5, LM Studio, ffmpeg, Whisper, CLIP, Chatterbox F5, and ComfyUI workflows. Dockerized each component and orchestrated them locally and in the cloud, exposing models through endpoints to generate video assets, without manual intervention.',
+        'img': [
+            '/imgs/projects/stockex/portfolio.jpg',
+            '/imgs/projects/stockex/profile.jpg',
+        ],
+    },
+    {
+        'tittle': 'Willy Trivia AI',
+        'description': 'A trivia AI that reads multiple choice questions from a phone screen with TESSERACT OCR and predicts the correct answer performing a google search, parsing the data and processing the information with a tf-idf based algorithm.',
+        'img': [
+            '/imgs/projects/stockex/panel.jpg',
+            '/imgs/projects/stockex/portfolio.jpg',
+            '/imgs/projects/stockex/profile.jpg',
+        ],
+    },
+]
